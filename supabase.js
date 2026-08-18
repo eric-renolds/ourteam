53d4392fde2eae0e4d8344ad0d4ffc1b60e691e4
eric-renolds <eric.renoldsnsolbpo@gmail.com>
2026-08-18 16:29:31 -0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,3 @@
 const SUPABASE_URL = "https://ezmwhhynnryzsxfjspwj.supabase.co";
-
 const SUPABASE_KEY = "sb_publishable_p4s7x6oxTbsMCWY31W037g_pgHZ-2AB";
-
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
